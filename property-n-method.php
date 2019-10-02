5fc599333f08bb2bc5273d9d7107d5bb18c4da09
Cherly Cheong <cherlycheong@gmail.com>
2019-10-03 01:06:59 +0700

    create property and method

---
+++ property-n-method.php
@@ -0,0 +1,23 @@
+<?php
+
+class Car {
+	public $name,
+	public $brand,
+	public $colour,
+	public $speedMax,
+	public $numPassenger;
+
+	public function addSpeed() {
+
+	}
+
+	public function cutSpeed() {
+
+	}
+
+	public function replaceTransmission() {
+
+	}
+
+}
+
